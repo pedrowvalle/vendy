f0122ccfaf003e313bebee1c38f800a30c6a2acc
Pedro Mompean Valle <pedrowvalle@gmail.com>
2020-04-06 16:08:52 -0300

    Ajuste do parametro dataN para dt_nsc a fim de alinhar com o banco.

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -4,25 +4,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.Cliente;
 
 public class ClienteDAO {
-	public Cliente incluir(Cliente cli) {
-		String SQLInsert = "INSERT INTO cliente (cpf,nome,datan,sexo) VALUES (?,?,?,?)";
+	public Cliente incluir(Cliente cli) throws ParseException {
+		java.sql.Date data;
+		String SQLInsert = "INSERT INTO cliente (cpf,nome,dt_nsc,genero) VALUES (?,?,?,?)";
+		DateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+		data = new java.sql.Date(fmt.parse(cli.getDatan()).getTime());
 		try(Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(SQLInsert);){
-			stm.setLong(1, cli.getCpf());
+			stm.setString(1, cli.getCpf());
 			stm.setString(2, cli.getNome());
-			stm.setString(3, cli.getDatan());
+			stm.setDate(3, data);
 			stm.setString(4, cli.getSexo());
 			stm.execute();
-			String sqlQuery = "SELECT LAST_INSERT_ID()";
-			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery);
-					ResultSet rs = stm2.executeQuery();) {
-				if(rs.next()) {
-					cli.setId(rs.getInt(1));
-				}
-			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -30,10 +30,10 @@ public class ClienteDAO {
 	}
 	
 	public void atualizar(Cliente cli) {
-		String SQLUpdate = "UPDATE cliente nome=?,datan=?,sexo=? WHERE cpf=?";
+		String SQLUpdate = "UPDATE cliente nome=?,dt_nsc=?,genero=? WHERE cpf=?";
 		try(Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(SQLUpdate);){
-			stm.setLong(1, cli.getCpf());
+			stm.setString(1, cli.getCpf());
 			stm.setString(2, cli.getNome());
 			stm.setString(3, cli.getDatan());
 			stm.setString(4, cli.getSexo());
@@ -47,7 +47,7 @@ public class ClienteDAO {
 		String SQLDelete = "DELETE FROM cliente WHERE cpf=?";
 		try(Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(SQLDelete);){
-			stm.setLong(1, cli.getCpf());
+			stm.setString(1, cli.getCpf());
 			stm.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -58,12 +58,12 @@ public class ClienteDAO {
 		String SQLSelect = "SELECT * FROM cliente WHERE cliente.cpf = ?";
 		try(Connection conn = ConnectionFactory.obtemConexao();
 				PreparedStatement stm = conn.prepareStatement(SQLSelect);){
-			stm.setLong(1, cli.getCpf());
+			stm.setString(1, cli.getCpf());
 			try(ResultSet rs = stm.executeQuery();){
 				if (rs.next()) {
 					cli.setNome(rs.getString("nome"));
-					cli.setDatan(rs.getString("datan"));
-					cli.setSexo(rs.getString("sexo"));
+					cli.setDatan(rs.getString("dt_nsc"));
+					cli.setSexo(rs.getString("genero"));
 				} 
 			} catch (SQLException e) {
 				e.printStackTrace();
