79ec2710d02faa970ef8b51430ce988cd2a8dbfe
Pedro Mompean Valle <pedrowvalle@gmail.com>
2020-05-19 18:15:38 -0300

    Pedidos agora são salvos no banco de dados.

--- src/command/DinheiroFinalizado.java
+++ src/command/DinheiroFinalizado.java
@@ -1,12 +1,18 @@
 package command;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import model.Empregado;
+import model.Pedido;
+import model.Produto;
+import service.PedidoService;
 
 public class DinheiroFinalizado implements Command {
 
@@ -25,8 +31,25 @@ public class DinheiroFinalizado implements Command {
 		Object cpf = request.getParameter("cpf");
 		session.setAttribute("cpfClienteFinalizado", cpf);
 		
+		Empregado emp = (Empregado)session.getAttribute("logado");
+		ArrayList <Produto> venda = (ArrayList<Produto>) session.getAttribute("venda");
+		String produtos = "";
+		java.util.Date date = new java.util.Date();
+		for(Produto p : venda) {
+			produtos += p.getNome();
+			produtos += "( " + p.getCont() + " )";
+			produtos += ";";
+		}
+		Pedido pedido = new Pedido();
+		pedido.setCod_produto(produtos);
+		pedido.setCpf_cliente(cpf.toString());
+		pedido.setPreco(total);
+		pedido.setData_pedido(date.toString());
+		pedido.setDesconto(total-desconto);
+		pedido.setVendedor(emp.getNome());
 		
-		
+		PedidoService ps = new PedidoService();
+		ps.incluir(pedido);
 		
 		view = request.getRequestDispatcher("caixa/venda-finalizada-dinheiro.jsp");
 		view.forward(request, response);
